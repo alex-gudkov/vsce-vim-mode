a67c2d7e277adbf6ad89e43bbfeebc190ad89c79
alex-gudkov <gudkov.bsuir@gmail.com>
2023-12-10 17:12:20 +0100

    update keybindings && lock typing while vim mode

--- extension.js
+++ extension.js
@@ -9,12 +9,19 @@ function activate(context) {
     Number.MIN_SAFE_INTEGER,
   );
 
+  let ignoreTypeCommand;
+
   const enableVimModeCommand = vscode.commands.registerCommand(
     'vsce-vim-mode.enableVimMode',
     async () => {
       vimModeStatusBar.text = '-- VIM MODE --';
       vimModeStatusBar.color = '#0dbc79';
       vimModeStatusBar.show();
+
+      ignoreTypeCommand = vscode.commands.registerCommand('type', () => {
+        return null;
+      });
+      context.subscriptions.push(ignoreTypeCommand);
 
       await vscode.commands.executeCommand('setContext', 'vsce-vim-mode.isVimModeEnabled', true);
     },
@@ -27,6 +34,10 @@ function activate(context) {
       vimModeStatusBar.hide();
 
       await vscode.commands.executeCommand('setContext', 'vsce-vim-mode.isVimModeEnabled', false);
+      
+      if (ignoreTypeCommand) {
+        ignoreTypeCommand.dispose();
+      }
     },
   );
 
